b2eec6d4852fb24356be545f4ef7d1c8bb72af05
JacobDChamberlain <straightlegmusic@gmail.com>
2022-03-31 16:02:13 -0500

    Phase 4: User Auth Routes - checkpoint - Login route works && git push

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
-const asyncHandler = require('express-async-handler');
-const { setTokenCookie } = require('../../utils/auth');
-const { User } = require('../../db/models');
+const sessionRouter = require('./session');
+const usersRouter = require('./users');
 
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
 
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 module.exports = router;
