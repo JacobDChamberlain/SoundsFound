8b74f37033bfbc153d9d99e82bdc7c82c435d5ac
JacobDChamberlain <straightlegmusic@gmail.com>
2022-03-31 13:19:56 -0500

    Phase 3: User Authentication - User table created & seeded with 3 demo users, checkpoint- completed model scopes

---
+++ backend/db/models/user.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const { Validator } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+      validate: {
+        len: [4, 30],
+        isNotEmail(value) {
+          if (Validator.isEmail(value)) {
+            throw new Error('Cannot be an email.');
+          }
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING(256),
+      allowNull: false,
+      validate: {
+        len: [3, 256]
+      }
+    },
+    hashedPassword: {
+      type: DataTypes.STRING.BINARY,
+      allowNull: false,
+      validate: {
+        len: [60, 60]
+      }
+    }
+  },
+  { // so when I query for User.findAll(), hashedPassword, email, createdAt, and updatedAt will not show up, etc.:
+    // (in other words, these help to protect sensitive user info that shouldn't be exposed or available to other users)
+    defaultScope: {
+      attributes: {
+        exclude: ['hashedPassword', 'email', 'createdAt', 'updatedAt']
+      }
+    },
+    scopes: {
+      currentUser: {
+        attributes: {
+          exclude: ['hashedPassword']
+        }
+      },
+      loginUser: {
+        attributes: {}
+      }
+    }
+  });
+
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
