6ee481bea95f4d5d8cb7825a10e3ae0da771c7fc
JacobDChamberlain <straightlegmusic@gmail.com>
2022-04-03 14:22:54 -0500

    back to working without songs table. restarting with plan

---
+++ backend/hold/old-demo-user-seed.js
@@ -0,0 +1,32 @@
+// 'use strict';
+
+// const bcrypt = require('bcryptjs');
+
+// module.exports = {
+//   up: (queryInterface, Sequelize) => {
+//     return queryInterface.bulkInsert('Users', [
+//       {
+//         email: 'demo@user.io',
+//         username: 'Demo-licious',
+//         hashedPassword: bcrypt.hashSync('password')
+//       },
+//       {
+//         email: 'user1@user.io',
+//         username: 'FakeMane1',
+//         hashedPassword: bcrypt.hashSync('password')
+//       },
+//       {
+//         email: 'user2@user.io',
+//         username: 'FakeMane2',
+//         hashedPassword: bcrypt.hashSync('password')
+//       },
+//     ], {});
+//   },
+
+//   down: (queryInterface, Sequelize) => {
+//     const Op = Sequelize.Op;
+//     return queryInterface.bulkDelete('Users', null, {
+//       username: { [Op.in]: ['Demo-licious', 'FakeMane1', 'FakeMane2'] }
+//     }, {});
+//   }
+// };
--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -3,19 +3,42 @@ const asyncHandler = require('express-async-handler');
 const { check } = require('express-validator');
 
 const { handleValidationErrors } = require('../../utils/validation');
-const { User } = require('../../db/models');
+const { Song } = require('../../db/models');
 
 const router = express.Router();
 
-const validateUploadSong = [
 
+const validateUploadSong = [
+  // TODO - validate song upload form fields
+  , handleValidationErrors
 ];
 
-// Get all songs (READ):
 
-// Upload a song: (CREATE): -- i have no idea if this works i'm just throwing spaghetti its 4:11am kbai
+// Get all songs (READ):
+router.get('/', asyncHandler(async (req, res) => {
+
+  const songs = await Song.findAll()
+
+  return res.json({ songs });
+}));
+
+
+// Get a single song song (READ):
+router.get('/:songId', asyncHandler(async (req, res) => {
+
+  const { songId } = req.params.songId;
+
+  const song = await Song.findByPk(songId);
+
+  return res.json({ song });
+}));
+
+
+// Upload a song: (CREATE):
 router.post('/', validateUploadSong, asyncHandler(async (req, res) => {
+
   const { userId, url, title } = req.body;
+
   const song = await Song.create({
     userId, url, title
   });
@@ -23,6 +46,28 @@ router.post('/', validateUploadSong, asyncHandler(async (req, res) => {
   return res.json({ song });
 }));
 
+
 // Edit a song (UPDATE):
+router.put('/', validateUploadSong, asyncHandler(async (req, res) => {
+
+  const { userId, url, title } = req.body;
+
+  const song = await Song.create({
+    userId, url, title
+  });
+
+  return res.json({ song });
+}));
+
 
 // Remove a song (DELETE):
+router.delete('/:songId', asyncHandler(async (req, res) => {
+
+  const { songId } = req.params.songId;
+
+  const song = await Song.findByPk(songId);
+
+  await song.delete();
+
+  return res.json({ "message": "song deleted. beep boop." }); // TODO - redirect user to home page
+}));
